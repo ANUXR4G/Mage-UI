8d869e5b9494380d9c93b9a24031552ceefa0aba
anuxr4g <anun20003@gmail.com>
2025-03-20 13:15:12 +0530

    feat: initial commit

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,6 +1,9 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
+import { IConfig } from 'next-sitemap';
+
+const config: IConfig = {
     siteUrl: 'https://mageui.live', // Replace with your actual domain
     generateRobotsTxt: true, // Generate a robots.txt file
     sitemapSize: 5000, // Default limit
 };
+
+export default config;
