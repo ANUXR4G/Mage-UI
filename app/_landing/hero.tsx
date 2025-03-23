ca95eca649f4e4d8504810be728cdf502ed03ebb
anuxr4g <anun20003@gmail.com>
2025-03-24 00:20:52 +0530

    feat: initial commit

--- app/_landing/hero.tsx
+++ app/_landing/hero.tsx
@@ -1,11 +1,9 @@
 import Link from "next/link";
-
 import { Icons } from "@/components/icons";
 import { PageHeaderDescription } from "@/components/page-header";
 import { buttonVariants } from "@/components/ui/button";
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
-
 
 import HeroTitle from "./hero-title";
 
@@ -18,7 +16,6 @@ function Pill({ children, className }: { children: React.ReactNode; className?: 
         className,
       )}
     >
-
       {children}
     </div>
   );
@@ -26,28 +23,27 @@ function Pill({ children, className }: { children: React.ReactNode; className?: 
 
 export default function Hero() {
   return (
-    <div className="relative  overflow-visible pt-16">
-
+    <div className="relative overflow-visible pt-16">
       <div
-        className="absolute inset-0 hidden aspect-square min-h-full w-[1200%] opacity-25 duration-1000 ease-in-out animate-in fade-in-0 dark:opacity-75 dark:mix-blend-soft-light dark:brightness-150 md:block md:w-[120%]"
+        className="absolute inset-0 aspect-square min-h-full w-full opacity-25 duration-1000 ease-in-out animate-in fade-in-0 dark:opacity-75 dark:mix-blend-soft-light dark:brightness-150"
         style={{
           backgroundImage: `radial-gradient(at 88% 11%, hsla(330,90%,65%,1) 0px, transparent 50%),
-radial-gradient(at 56% 93%, hsla(220,80%,30%,1) 0px, transparent 50%),
-radial-gradient(at 87% 28%, hsla(330,90%,65%,1) 0px, transparent 50%),
-radial-gradient(at 75% 83%, hsla(310,85%,70%,1) 0px, transparent 50%),
-radial-gradient(at 33% 39%, hsla(220,80%,30%,1) 0px, transparent 50%),
-radial-gradient(at 62% 69%, hsla(275,88%,68%,1) 0px, transparent 50%),
-radial-gradient(at 66% 84%, hsla(300,90%,65%,1) 0px, transparent 50%)`,
+          radial-gradient(at 56% 93%, hsla(220,80%,30%,1) 0px, transparent 50%),
+          radial-gradient(at 87% 28%, hsla(330,90%,65%,1) 0px, transparent 50%),
+          radial-gradient(at 75% 83%, hsla(310,85%,70%,1) 0px, transparent 50%),
+          radial-gradient(at 33% 39%, hsla(220,80%,30%,1) 0px, transparent 50%),
+          radial-gradient(at 62% 69%, hsla(275,88%,68%,1) 0px, transparent 50%),
+          radial-gradient(at 66% 84%, hsla(300,90%,65%,1) 0px, transparent 50%)`,
         }}
       />
       <div className="container relative overflow-hidden py-16 md:py-48">
         <div className="relative mx-auto flex max-w-7xl flex-col items-center justify-center gap-8 px-4 md:flex-row">
           <div className="flex flex-col items-center justify-center gap-2">
             <HeroTitle />
-
             <PageHeaderDescription className="relative mx-auto mb-4 w-fit duration-1000 ease-minor-spring animate-in fade-in-0 slide-in-from-bottom-3">
               Bring your ideas to Code with <br />
-              <span className="underline decoration-wavy underline-offset-4 mt-2">✨ beautifully crafted animations and effects</span><br /> <p className="mt-2"> —designed to transform your projects,</p>{" "}
+              <span className="underline decoration-wavy underline-offset-4 mt-2">✨ beautifully crafted animations and effects</span><br />
+              <p className="mt-2"> —designed to transform your projects,</p>{" "}
               <p className="underline decoration-wavy underline-offset-4 -mt-6"><br />one copy-paste at a time!</p>
             </PageHeaderDescription>
             <div className="relative mb-6 mt-3 flex items-start gap-4 duration-1000 ease-minor-spring animate-in fade-in-0 slide-in-from-bottom-3">
@@ -67,6 +63,7 @@ radial-gradient(at 66% 84%, hsla(300,90%,65%,1) 0px, transparent 50%)`,
           </div>
         </div>
 
+        {/* Pill components */}
         <Pill className="left-12 top-20 rotate-3">200+ components</Pill>
         <Pill className="right-12 top-20 -translate-y-1/2 -rotate-6">Animations</Pill>
         <Pill className="bottom-12 left-12 -rotate-6">Ready To Use</Pill>
@@ -83,9 +80,11 @@ radial-gradient(at 66% 84%, hsla(300,90%,65%,1) 0px, transparent 50%)`,
         <Pill className="right-12 top-32 aspect-square translate-y-full p-1">
           <Icons.yarn className="size-10" />
         </Pill>
-        <Pill className="left-12 top-32 aspect-square translate-y-full p-2">
-          <Icons.reactJS className="size-8" />
+        <Pill className="left-12 top-32 aspect-square translate-y-full p=2">
+          <Icons.reactJS className="size=8" />
         </Pill>
+
+        {/* Noise filter */}
         <svg className="hidden">
           <filter id="noise">
             <feTurbulence
